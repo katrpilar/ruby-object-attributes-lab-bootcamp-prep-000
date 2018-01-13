e7ab81f63e48ce5292e4b200795938e186f2bcd5
Katrina Brinson <kbrinso@gmail.com>
2018-01-13 02:44:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-  def initialize()
+  def initialize(p_name, p_job)
+    @p_job = p_job
+    @p_name = p_name
+  end
+  
+  def name=(p_name)
+    @p_name = p_name
+  end
+  
+  def name(p_name)
+  end
+  
+  def job(p_job)
+    @p_job = p_job
+  end
+  
+  def job(p_job)
+  end
 end
